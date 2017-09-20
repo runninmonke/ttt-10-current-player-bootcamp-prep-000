204ef72625c86502e1da613414aa172a9c4b0999
Ryan Pfeifer <pfeifer.ryan@gmail.com>
2017-09-20 19:11:07 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,10 +5,18 @@ end
 def turn_count(board)
   count = 0
   board.each do |location|
-    if(position_taken?(board, location))
+    if position_taken?(board, location)
       count += 1
     end
   end
   count
+end
+
+def current_player(board)
+  if turn_count == 0 || turn_count%2 == 0
+    "X"
+  else
+    "0"
+  end
 end
     
